063e0f2e5b998896f72f27bce66cf0e1e15f4109
Olalekan Rasaq <69814366+Olalekanrasaq@users.noreply.github.com>
2026-04-09 06:41:46 +0100

    Add loan_task.py for displaying loan targets
    Implement loan task display for targeted businesses.

---
+++ tasks/loan_task.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+from data.loader import load_products, load_businesses_users, load_data
+from streamlit_gsheets import GSheetsConnection
+
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+users = load_businesses_users()["users"]
+business_df = load_businesses_users()["business"]
+df_transc = load_data()["df_transc_today"]
+
+st.title("Loans Target")
+st.write("Businesses targeted for loans")
+
+merged_df = get_loan_tasks(users, business_df, df_transc_today)
+
+staff_dfs = {
+    staff: df.reset_index(drop=True)
+    for staff, df in merged_df.groupby("Assigned_staff")
+}
+
+selected_staff = st.selectbox("Select Staff", staff_dfs.keys())
+
+st.dataframe(
+    staff_dfs[selected_staff],
+    hide_index=True,
+    use_container_width=True
+)
